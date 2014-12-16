fced3aadbdc4872468b79b2c9afcfe7a0c87b728
Shaun Mulligan <switchflip.shaun@gmail.com>
2014-12-16 12:37:42 +0200

    added libraspberrypi-bin to deps

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+var RaspiCam = require("raspicam");
+
+var camera = new RaspiCam({
+	mode: "photo",
+	output: "../data/image.jpg",
+	encoding: "jpg",
+	timeout: 0 // take the picture immediately
+});
+
+camera.on("started", function( err, timestamp ){
+	console.log("photo started at " + timestamp );
+});
+
+camera.on("read", function( err, timestamp, filename ){
+	console.log("photo image captured with filename: " + filename );
+});
+
+camera.on("exit", function( timestamp ){
+	console.log("photo child process has exited at " + timestamp );
+});
+
+camera.start();
+
 var users = {};
 if(process.env.TTYUSER && process.env.TTYPASSWORD) {
 	users[process.env.TTYUSER] = process.env.TTYPASSWORD;
