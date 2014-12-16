cb22f1c29501b954eb24746c55147da769e8b18e
Shaun Mulligan <switchflip.shaun@gmail.com>
2014-12-16 12:58:48 +0200

    comment out tty.js stuff

--- app.js
+++ app.js
@@ -21,12 +21,12 @@ camera.on("exit", function( timestamp ){
 
 camera.start();
 
-var users = {};
-if(process.env.TTYUSER && process.env.TTYPASSWORD) {
-	users[process.env.TTYUSER] = process.env.TTYPASSWORD;
-}
-require('tty.js').createServer({
-	shell: 'bash',
-	users: users,
-	port: process.env.TTYPORT || process.env.PORT
-}).listen();
+// var users = {};
+// if(process.env.TTYUSER && process.env.TTYPASSWORD) {
+// 	users[process.env.TTYUSER] = process.env.TTYPASSWORD;
+// }
+// require('tty.js').createServer({
+// 	shell: 'bash',
+// 	users: users,
+// 	port: process.env.TTYPORT || process.env.PORT
+// }).listen();
